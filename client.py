bae4a55a9ababff8e36564761848477b751a4080
KaelAnderson <114518738+KaelAnderson@users.noreply.github.com>
2023-10-15 21:04:45 -0400

    Created server and client with reversing

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+def main():
+    # make a socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect to server using ip and port
+    host = '127.0.0.1'  
+    port = 30060         
+    client_socket.connect((host, port))
+
+    while True:
+        message = input("Enter a message ('end' to exit): ")
+        
+
+        # Send the message to the server
+        client_socket.send(message.encode('utf-8'))
+
+        # Receive and print the reversed message from the server
+        reversed_message = client_socket.recv(1024).decode('utf-8')
+
+        print(f"Server Response: {reversed_message}")
+        if reversed_message == "dne":
+            client_socket.close()
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+
+def reverse_string(input_string):
+    return input_string[::-1]   #this reverses the string using regex
+
+def main():
+    # create the socket for the server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # gives the socket its connection info
+    host = '127.0.0.1'  # host ip adress
+    port = 30060
+    server_socket.bind((host, port))
+
+    # listen for calls
+    server_socket.listen(5)
+    print(f"Server is listening on {host}:{port}")
+
+    while True:
+        # connect with client
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr}")
+
+        while True:
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+            else:
+                reversed_data = reverse_string(data) #call the reverse string function
+                client_socket.send(reversed_data.encode('utf-8')) # send back the reversed data    
+                if data == 'end':
+                     client_socket.close()
+                     print(f"Connection with {addr} closed")
+            
+
+
+if __name__ == '__main__':
+    main()
